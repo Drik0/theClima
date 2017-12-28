f65a6f65bbdac72064c2a3d64fd27022fe7be64d
Gerson <gerson.lcc@hotmail.com>
2017-12-28 16:07:33 +0000

    Weather Data Model, Outlets and Actions

--- TheClima/Controller/WeatherVC.swift
+++ TheClima/Controller/WeatherVC.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class WeatherVC: UIViewController {
 
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var iconImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func switchBtnPressed(_ sender: UIButton) {
     }
-
-
+    
+    
 }
 
---
+++ TheClima/Model/WeatherDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherDataModel.swift
+//  TheClima
+//
+//  Created by Gerson Costa on 28/12/2017.
+//  Copyright © 2017 Gerson Costa. All rights reserved.
+//
+
+import UIKit
+
+class WeatherDataModel {
+    
+}
